b50384f5cb1e5d8788ffd3e7a0d4c816c798be9b
Jinming-Chen-ICD2O1-01 <jinming.chen@student.tdsb.on.ca>
2024-11-08 09:49:17 -0500

    Update PP1_7.py

--- PP1_7.py
+++ PP1_7.py
@@ -1,25 +1,38 @@
-
+'''
+    Lesson: Booleans
+    Author: Jinming Chen
+    Date Creatd: Nove 08, 2024
+    Date Last Modified: Nove 08, 2024
+'''
 
 q1():
   #Write code here
-
+print(True)
 q2():
   #Write code here
-
+num = input("Input an integer: ")
+print(num > 5)
 q3():
   #Write code here
-
+word = input("Enter the letter a: ")
+print(word == "a")
 q4():
   #Write code here
-
+word = input("Enter a word in the dictionary that is earlier than google: ")
+print(word < "google")
 q5():
   #Write code here
-
+num2 = int(input("Input an integer: "))
+num3 = int(input("Enter another integer: "))
+product = num2 * num3
+print(f"Your numbers multiplied together are greater than 40: {product > 40}")
 #Do edit the code below
 #Comment the lines below when running your tests
 
-q1()
-q2()
-q3()
-q4()
-q5()
+q1()# Outputs a boolean value of True
+
+q2()# Ask the user to "Input an integer:". Outputs True when input is greater than 5. False if not.
+q3()# Ask the user to "Enter the letter a:". ​​Output True when input is a. Output False if it is not a.
+q4()# Ask the user, "Enter a word in the dictionary that is earlier than google:". Output True if the input is earlier than google in the dictionary. Output False if it is not.
+q5()# Ask the user to "Input an integer:". Ask the user to "Enter another integer:".
+# Output "Your numbers multiplied together are greater than 40:", followed by True if they are greater than 40, or False if they are not greater than 40.
